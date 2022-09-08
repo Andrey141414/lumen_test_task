1d4c5ea7086e7613d7be6022fdad45767d1c6aa7
Андрей Винокуров <andrusha.vinokurov@gmail.com>
2022-09-08 14:54:17 +0700

    add hollidays

--- app/Http/Controllers/Task2.php
+++ app/Http/Controllers/Task2.php
@@ -48,6 +48,7 @@ class Task2 extends Controller
         'direction'=>'required|string',
     ];
 
+public $hollidays = ['01.01', '08.03', '09.05','23.03'];
 
 public function logic_system_modul(HttpRequest $request)
     {
@@ -81,41 +82,47 @@ public function logic_system_modul(HttpRequest $request)
         for($i = 0;$i<$day_count;$i++)
         {
             $date->addDay(1);
-            if($direction == $this->direction[0] || $direction == $this->direction[1] )
+            if( array_search($date->format('d.m'),$this->hollidays))
             {
-                if(
-                $date->dayOfWeek== array_search('Понедельник',$this->weekMap)||
-                $date->dayOfWeek== array_search('Среда',$this->weekMap)||
-                $date->dayOfWeek== array_search('Пятница',$this->weekMap)
-                )
-                {
-                    if(($i == 0 && $time > Carbon::parse('16:00')) == false)
-                    {
-                        array_push($result,(['date'=>$date->format('d.m.y'),
-                        'day'=>$this->weekMap[$date->dayOfWeek],
-                        'title' =>$date->format('d M')
-                        ]));    
-                    }
-                    
-                 }
-            }
 
-            else
-            {
-                if(
-                    $date->dayOfWeek== array_search('Вторник',$this->weekMap)||
-                    $date->dayOfWeek== array_search('Четверг',$this->weekMap)||
-                    $date->dayOfWeek== array_search('Суббота',$this->weekMap)
+            }
+            else{
+                if($direction == $this->direction[0] || $direction == $this->direction[1] )
+                {
+                    if(
+                    $date->dayOfWeek== array_search('Понедельник',$this->weekMap)||
+                    $date->dayOfWeek== array_search('Среда',$this->weekMap)||
+                    $date->dayOfWeek== array_search('Пятница',$this->weekMap)
                     )
                     {
-                        if(($i == 0 && $time > Carbon::parse('20:00')) == false)
-                            {
+                        if(($i == 0 && $time > Carbon::parse('16:00')) == false)
+                        {
                             array_push($result,(['date'=>$date->format('d.m.y'),
                             'day'=>$this->weekMap[$date->dayOfWeek],
                             'title' =>$date->format('d M')
-                            ]));
+                            ]));    
                         }
-                     }
+                        
+                    }
+                }
+
+                else
+                {
+                    if(
+                        $date->dayOfWeek== array_search('Вторник',$this->weekMap)||
+                        $date->dayOfWeek== array_search('Четверг',$this->weekMap)||
+                        $date->dayOfWeek== array_search('Суббота',$this->weekMap)
+                        )
+                        {
+                            if(($i == 0 && $time > Carbon::parse('20:00')) == false)
+                                {
+                                array_push($result,(['date'=>$date->format('d.m.y'),
+                                'day'=>$this->weekMap[$date->dayOfWeek],
+                                'title' =>$date->format('d M')
+                                ]));
+                            }
+                        }
+                }
             }
         }
         return response()->json( $result);
